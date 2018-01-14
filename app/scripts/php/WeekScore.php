c7c0695a50831a30018ac21f022e7cfdfdc96a94
Owen Doylem <owentdoyler@gmail.com>
2018-01-14 20:30:58 +0000

    lowest score of week logic

--- app/scripts/php/WeekScore.php
+++ app/scripts/php/WeekScore.php
@@ -4,16 +4,28 @@
         public $scores;
         public $amountCounting;
         public $min_score;
-        public $flag; /// flag if number of scores didn't reach the minimum
-        public $overrideScore; // this will be populated with the mimimum score in the given week if the team hasnt fielded enough players
+        // public $flag; /// flag if number of scores didn't reach the minimum
+        // public $overrideScore; // this will be populated with the mimimum score in the given week if the team hasnt fielded enough players
 
-        public function __construct($weekNumber, $scores, $amountCounting){
+        public function __construct($weekNumber, $scores, $amountCounting, $min_score){
             $this->weekNumber = $weekNumber;
             $this->scores = $scores;
             $this->amountCounting = $amountCounting;
-            $this->flag = false;
-            $this->overrideScore = 0;
+            $this->min_score = $min_score;
+            // $this->flag = false;
+            // $this->overrideScore = 0;
+            $this->fillSupplementScores();
             $this->sortScores();
+        }
+
+        private function fillSupplementScores(){
+            $scores_amount = sizeof($this->scores);
+            if($scores_amount < $this->amountCounting){
+                while($score_amount <=  $this->amountCounting){
+                    array_push($this->scores, new Score("Supplementary"), $this->min_score, 0);
+                    $score_amount++;
+                }
+            }
         }
 
         private function sortScores(){
@@ -33,16 +45,16 @@
 
         public function weekScore(){
 
-            if($this->overrideScore > 0){
-                return $this->overrideScore;
-            }
-            else{
+            // if($this->overrideScore > 0){
+            //     return $this->overrideScore;
+            // }
+            // else{
                 $total = 0;
                 foreach($this->countingScores() as $countingScore){
                     $total += $countingScore->nettScore;
                 }
                 return $total;
-            }
+            // }
         }
 
         public function toJson(){
